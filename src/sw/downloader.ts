2d18777ddab23c2483732104ba5912504059bd3b
Misty De Meo <mistydemeo@gmail.com>
2026-06-22 15:33:07 -0700

    chore: remove unnecessary eslint comments

--- src/sw/downloader.ts
+++ src/sw/downloader.ts
@@ -353,7 +353,7 @@ class Downloader {
     //let count = 0;
 
     while (resources.length) {
-      const last: ResourceEntry = resources[resources.length - 1]!;
+      const last: ResourceEntry = resources[resources.length - 1];
 
       if (this.pageList) {
         resources = resources.filter((res) =>
@@ -517,7 +517,7 @@ class Downloader {
 
         // response record
         const responseData: { length: number; digest?: string } = { length: 0 };
-        yield* this.emitRecord(records[0]!, digestRecordAndCDX, responseData);
+        yield* this.emitRecord(records[0], digestRecordAndCDX, responseData);
         offset += responseData.length;
         resource.length = responseData.length;
         if (digestRecordAndCDX && !resource.recordDigest) {
@@ -528,7 +528,7 @@ class Downloader {
         // request record, if any
         if (records.length > 1) {
           const requestData = { length: 0 };
-          yield* this.emitRecord(records[1]!, false, requestData);
+          yield* this.emitRecord(records[1], false, requestData);
           offset += requestData.length;
         }
 
--- src/sw/ipfsutils.ts
+++ src/sw/ipfsutils.ts
@@ -212,8 +212,7 @@ async function ipfsWriteBuff(
 ) {
   const file = UnixFS.createFileWriter(writer);
   if (content instanceof Uint8Array) {
-    await file.write(content);
-    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+    await file.write(content); 
   } else if (content[Symbol.asyncIterator]) {
     for await (const chunk of content) {
       await file.write(chunk);
@@ -347,7 +346,6 @@ async function splitByWarcRecordGroup(
         links = [];
       }
 
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
       fileLinks.push(link);
 
       const [dirName, filename] = getDirAndName(currName);
@@ -364,7 +362,6 @@ async function splitByWarcRecordGroup(
         dir = dirs[dirName];
       }
 
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
       dir.set(filename, link);
 
       inZipFile = false;
@@ -378,8 +375,7 @@ async function splitByWarcRecordGroup(
         count = 0;
         file = UnixFS.createFileWriter(writer);
 
-        if (chunk === WARC_GROUP) {
-          // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+        if (chunk === WARC_GROUP) { 
           secondaryLinks.push(await concat(writer, links));
           links = [];
         }
@@ -416,7 +412,6 @@ async function splitByWarcRecordGroup(
 
   rootDir.set("webarchive", await waczDir.close());
 
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
   rootDir.set(waczPath, await concat(writer, fileLinks));
 }
 
@@ -428,8 +423,7 @@ async function concat(
   //TODO: is this the right way to do this?
   const { fileEncoder, hasher, linker } = writer.settings;
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const advanced = (fileEncoder as any).createAdvancedFile(links);
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+  const advanced = (fileEncoder as any).createAdvancedFile(links); 
   const bytes = fileEncoder.encode(advanced);
   const hash = await hasher.digest(bytes);
   const cid = linker.createLink(fileEncoder.code, hash);
@@ -448,8 +442,7 @@ async function concat(
 }
 
 export const iterate = async function* (stream: ReadableStream<Uint8Array>) {
-  const reader = stream.getReader();
-  // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+  const reader = stream.getReader(); 
   while (true) {
     const next = await reader.read();
     if (next.done) {
@@ -461,8 +454,7 @@ export const iterate = async function* (stream: ReadableStream<Uint8Array>) {
 };
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export async function encodeBlocks(blocks: UnixFS.Block[], root?: any) {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+export async function encodeBlocks(blocks: UnixFS.Block[], root?: any) { 
   const { writer, out } = CarWriter.create(root);
   /** @type {Error?} */
   let error;
@@ -479,8 +471,7 @@ export async function encodeBlocks(blocks: UnixFS.Block[], root?: any) {
     }
   })();
   const chunks = [];
-  for await (const chunk of out) chunks.push(chunk);
-  // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+  for await (const chunk of out) chunks.push(chunk); 
   if (error != null) throw error;
   const roots = root != null ? [root] : [];
   console.log("chunks", chunks.length);
@@ -557,8 +548,7 @@ export class ShardingStream extends TransformStream {
           readySize = currSize;
           shard = [];
           currSize = 0;
-        }
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+        } 
         shard.push(block);
         currSize += block.bytes.length;
       },
